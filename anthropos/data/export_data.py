cc43e20436a3f057a62d0b0dce3eab86fc989b9a
Anton Strokov <anton.strokoff@gmail.com>
2023-07-25 16:55:48 +0300

    minor fixes

---
+++ anthropos/data/export_data.py
@@ -0,0 +1,37 @@
+from anthropos.models import Individ
+# from typing import List
+from os import path, remove
+from flask import Flask
+import pandas as pd
+
+def export_xls(individs: list[Individ], current_app: Flask, export_name: str ='default') -> str:
+
+    export_data: dict = dict()
+
+    export_data.setdefault('Индекс', [individ.index for individ in individs])
+    export_data.setdefault('Памятник', [individ.site for individ in individs])
+    export_data.setdefault('Погребение', [individ.grave for individ in individs])
+    export_data.setdefault('Эпохи', [", ".join([f'{epoch}'for epoch in individ.site.epochs]) for individ in individs])
+    export_data.setdefault('Исследователь', [individ.site.researcher for individ in individs])
+    export_data.setdefault('Расположение', [individ.site.regions for individ in individs])
+    export_data.setdefault('Долгота', [individ.site.long for individ in individs])
+    export_data.setdefault('Широта', [individ.site.lat for individ in individs])
+    export_data.setdefault('Год', [individ.year for individ in individs])
+    export_data.setdefault('Возраст', [f'{individ.age_min}-{individ.age_max}' for individ in individs])
+    export_data.setdefault('Обряд', [individ.type for individ in individs])
+    export_data.setdefault('Пол', [individ.sex for individ in individs])
+    export_data.setdefault('Сохранность', [individ.preservation for individ in individs])
+    export_data.setdefault('Примечание', [individ.comment for individ in individs])
+    export_data.setdefault('Кем создано', [individ.creator for individ in individs])
+    export_data.setdefault('Создано', [individ.created_at for individ in individs])
+    export_data.setdefault('Кем изменено', [individ.editor for individ in individs])
+    export_data.setdefault('Изменено', [individ.edited_at for individ in individs])
+
+    path_to_file: str = path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER'], f'{export_name}.xlsx')
+    if path.isfile(path_to_file):
+        remove(path_to_file)
+    df = pd.DataFrame(export_data, [pd.Index(range(1, len(individs) + 1))])
+    df['Создано'] = df['Создано'].dt.tz_localize('UTC').dt.tz_convert('Europe/Moscow').dt.tz_localize(None)
+    df['Изменено'] = df['Изменено'].dt.tz_localize('UTC').dt.tz_convert('Europe/Moscow').dt.tz_localize(None)
+    df.to_excel(path_to_file, sheet_name=export_name)
+    return path_to_file
--- anthropos/data/routes.py
+++ anthropos/data/routes.py
@@ -4,13 +4,8 @@ from anthropos import db
 from .forms import FilterForm
 from anthropos.data import bp
 from anthropos.models import Sex, Individ, Researcher, ArchaeologicalSite, Epoch, FederalDistrict, Region, Preservation, Grave
-from datetime import datetime
 from sqlalchemy import select
-import pandas as pd
-from os import path
-# from reportlab.pdfgen.canvas import Canvas
-# from reportlab.lib.pagesizes import A4
-
+from anthropos.data.export_data import export_xls
 
 
 @bp.route('/individ_data', methods=['GET', 'POST'])
@@ -18,38 +13,12 @@ from os import path
 def individ_data():
     individs = Individ.get_all(db.session)
     form = FilterForm()
-    for i in individs:
-        if i.id == 28:
-            print(i.file)
+    a = 'kek'
+    fn = 123
     if request.method == 'POST':
-
-        export_data = dict()
-
-        export_data.setdefault('Индекс', [individ.index for individ in individs])
-        export_data.setdefault('Памятник', [individ.site for individ in individs])
-        export_data.setdefault('Погребение', [individ.grave for individ in individs])
-        export_data.setdefault('Эпохи', [", ".join([f'{epoch}'for epoch in individ.site.epochs]) for individ in individs])
-        export_data.setdefault('Исследователь', [individ.site.researcher for individ in individs])
-        export_data.setdefault('Расположение', [individ.site.regions for individ in individs])
-        export_data.setdefault('Долгота', [individ.site.long for individ in individs])
-        export_data.setdefault('Широта', [individ.site.lat for individ in individs])
-        export_data.setdefault('Год', [individ.year for individ in individs])
-        export_data.setdefault('Возраст', [f'{individ.age_min}-{individ.age_max}' for individ in individs])
-        export_data.setdefault('Обряд', [individ.type for individ in individs])
-        export_data.setdefault('Пол', [individ.sex for individ in individs])
-        export_data.setdefault('Сохранность', [individ.preservation for individ in individs])
-        export_data.setdefault('Примечание', [individ.comment for individ in individs])
-        export_data.setdefault('Кем создано', [individ.creator for individ in individs])
-        export_data.setdefault('Создано', [individ.created_at for individ in individs])
-        export_data.setdefault('Кем изменено', [individ.editor for individ in individs])
-        export_data.setdefault('Изменено', [individ.edited_at for individ in individs])
-
-        path_to_file = path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER'], 'all_individs.xlsx')
-        df = pd.DataFrame(export_data, [pd.Index(range(1, len(individs) + 1))])
-        df['Создано'] = df['Создано'].dt.tz_localize('UTC').dt.tz_convert('Europe/Moscow').dt.tz_localize(None)
-        df['Изменено'] = df['Изменено'].dt.tz_localize('UTC').dt.tz_convert('Europe/Moscow').dt.tz_localize(None)
-        df.to_excel(path_to_file, sheet_name='all_individs')
-        return send_file(path_to_file, as_attachment=True)
+        file = export_xls(individs, current_app, export_name='all_individs')
+        aaa = export_xls(a, current_app, export_name=fn)
+        return send_file(file, as_attachment=True)
     return render_template('data_output.html', individs=individs, form=form, action=url_for('data.individ_data'))
 
 
@@ -58,75 +27,34 @@ def individ_data():
 def search():
     form = FilterForm()
     if request.args:
-        b = request.args
-        args_dict = dict()
-        for i in b:
-            temp = b.get(i)
-            if i in ('year_min', 'year_max'):
-                args_dict.setdefault(i, temp)
-            if temp != '__None':
-                args_dict.setdefault(i, b.getlist(i))
+        filters = dict()
+        for argument in request.args:
+            value = request.args.get(argument)
+            if argument in ('year_min', 'year_max'):
+                filters.setdefault(argument, value)
+            if value != '__None':
+                filters.setdefault(argument, request.args.getlist(argument))
         stmt = select(Individ).join(Individ.site).join(Individ.preservation).join(ArchaeologicalSite.researcher).join(Individ.sex).join(Individ.editor).join(ArchaeologicalSite.regions)
-        if a := args_dict.get('epoch'):
-            stmt = stmt.join(ArchaeologicalSite.epochs).where(getattr(Epoch, 'id').in_(args_dict.get('epoch')))
-        if b := args_dict.get('researcher'):
-            stmt = stmt.where(getattr(Researcher, 'id').in_(args_dict.get('researcher')))
-        if c := args_dict.get('federal_district'):
-            stmt = stmt.join(Region.federal_district).where(getattr(FederalDistrict, 'id').in_(args_dict.get('federal_district')))
-        if d := args_dict.get('year_min'):
+        if a := filters.get('epoch'):
+            stmt = stmt.join(ArchaeologicalSite.epochs).where(getattr(Epoch, 'id').in_(a))
+        if b := filters.get('researcher'):
+            stmt = stmt.where(getattr(Researcher, 'id').in_(b))
+        if c := filters.get('federal_district'):
+            stmt = stmt.join(Region.federal_district).where(getattr(FederalDistrict, 'id').in_(c))
+        if d := filters.get('year_min'):
             stmt = stmt.where(Individ.year >= d)
-        if e := args_dict.get('year_max'):
+        if e := filters.get('year_max'):
             stmt = stmt.where(Individ.year <= e)
-        if f := args_dict.get('sex'):
+        if f := filters.get('sex'):
             stmt = stmt.where(getattr(Sex, 'sex').in_(f))
-        if g := args_dict.get('preservation'):
+        if g := filters.get('preservation'):
             stmt = stmt.where(getattr(Preservation, 'id').in_(g))
-        if e := args_dict.get('grave_type'):
+        if e := filters.get('grave_type'):
             stmt = stmt.join(Individ.grave).where(getattr(Grave, 'grave_type').in_(e))
-        print(stmt)
         global individs
-        individs = db.session.scalars(stmt.group_by(Individ.id)).all()
-        
+        individs = db.session.scalars(stmt.group_by(Individ.id)).all() 
         return render_template('data_output.html', individs=individs, form=form, action=url_for('data.search'))
     if request.method == 'POST':
-        export_data = dict()
-        export_data.setdefault('Индекс', [individ.index for individ in individs])
-        export_data.setdefault('Памятник', [individ.site for individ in individs])
-        export_data.setdefault('Погребение', [individ.grave for individ in individs])
-        export_data.setdefault('Эпохи', [", ".join([f'{epoch}'for epoch in individ.site.epochs]) for individ in individs])
-        export_data.setdefault('Исследователь', [individ.site.researcher for individ in individs])
-        export_data.setdefault('Расположение', [individ.site.regions for individ in individs])
-        export_data.setdefault('Долгота', [individ.site.long for individ in individs])
-        export_data.setdefault('Широта', [individ.site.lat for individ in individs])
-        export_data.setdefault('Год', [individ.year for individ in individs])
-        export_data.setdefault('Возраст', [f'{individ.age_min}-{individ.age_max}' for individ in individs])
-        export_data.setdefault('Обряд', [individ.type for individ in individs])
-        export_data.setdefault('Пол', [individ.sex for individ in individs])
-        export_data.setdefault('Сохранность', [individ.preservation for individ in individs])
-        export_data.setdefault('Примечание', [individ.comment for individ in individs])
-        export_data.setdefault('Кем создано', [individ.creator for individ in individs])
-        export_data.setdefault('Создано', [individ.created_at for individ in individs])
-        export_data.setdefault('Кем изменено', [individ.editor for individ in individs])
-        export_data.setdefault('Изменено', [individ.edited_at for individ in individs])
-
-        path_to_file = path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER'], 'filtered_individs.xlsx')
-        df = pd.DataFrame(export_data, [pd.Index(range(1, len(individs) + 1))])
-        df['Создано'] = df['Создано'].dt.tz_localize('UTC').dt.tz_convert('Europe/Moscow').dt.tz_localize(None)
-        df['Изменено'] = df['Изменено'].dt.tz_localize('UTC').dt.tz_convert('Europe/Moscow').dt.tz_localize(None)
-        df.to_excel(path_to_file, sheet_name='filtered_individs')
-        return send_file(path_to_file, as_attachment=True)
+        file = export_xls(individs, current_app, export_name='filtered_individs')
+        return send_file(file, as_attachment=True)
     return redirect(url_for('data.individ_data'))
-
-
-# @bp.route('/pdf', methods=['GET', 'POST'])
-# @login_required
-# def pdf():
-#     individs = Individ.get_all(db.session)
-#     for individ in individs:
-#         c = Canvas(f"/Users/antonstrokov/VSCode/ia_ras_anthropology/anthropos/static/pdfs/{individ.index}.pdf", pagesize=A4)
-#         c.drawString(5, 5, f"{individ.index} - {individ.site} - лолка проверка")
-#         c.line(20, 20, 50, 20)
-
-
-#         c.save()
-#     return 'ok'
