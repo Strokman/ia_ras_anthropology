4f7d050447070078df5a41aeebf9d431e0758eb8
Anton Strokov <anton.strokoff@gmail.com>
2023-08-03 17:23:22 +0300

    add file deletion and download name fix

--- anthropos/file/routes.py
+++ anthropos/file/routes.py
@@ -1,15 +1,26 @@
-from flask import send_file, flash, redirect, url_for, current_app
-from os import path
+from flask import send_file, flash, redirect, url_for
+from os import path, remove
+from anthropos.extensions import db
 from anthropos.file import bp
+from anthropos.models import File
 
 
 @bp.route('/file/<filename>')
 def file(filename):
-    path_to_file = path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER'], filename)
-    if path.isfile(path_to_file):
-        if path_to_file.split('.')[1] == 'pdf':
-            return send_file(path_to_file)
-        return send_file(path_to_file, as_attachment=True)
-    else:
-        flash('Файл не существует', 'warning')
-        return redirect(url_for('individ.individ_table'))
+    file: File = File.get_one_by_attr(File.filename, filename)
+    if file and path.isfile(file.path) and file.extension == 'pdf':
+        return send_file(file.path, download_name=f'{file.individ.index}.{file.extension}')
+    elif file and file.extension != 'pdf':
+        return send_file(file.path, as_attachment=True, download_name=f'{file.individ.index}.{file.extension}')
+    flash('Файл не существует', 'warning')
+    return redirect(url_for('individ.individ_table'))
+
+
+@bp.route('/delete_file/<filename>')
+def delete_file(filename):
+    file: File = File.get_one_by_attr(File.filename, filename)
+    if file:
+        remove(file.path)
+        db.session.delete(file)
+        db.session.commit()
+    return redirect(url_for('individ.individ_table'))
