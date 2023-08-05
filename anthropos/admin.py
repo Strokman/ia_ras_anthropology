446db3aebb10d7b84cb83f8e73f493c3d7276a59
Anton Strokov <anton.strokoff@gmail.com>
2023-08-05 09:36:07 +0300

    fix issue with admin views

--- anthropos/admin.py
+++ anthropos/admin.py
@@ -6,13 +6,29 @@ from anthropos.models import (
     ArchaeologicalSite,
     DatabaseUser,
     Region,
-    Epoch
+    Epoch,
+    Individ,
+    Preservation,
+    Comment,
+    FederalDistrict,
+    File,
+    Grave,
+    Researcher,
+    Sex
 )
 
 
 admin = Admin(name='BaseHabilis', template_mode='bootstrap4')
 
-admin.add_view(ModelView(DatabaseUser, db.session))
-admin.add_view(ModelView(ArchaeologicalSite, db.session))
+admin.add_view(ModelView(DatabaseUser, db.session, name='Users', url='users'))
+admin.add_view(ModelView(Individ, db.session, name='Individs', url='individs', endpoint='individs'))
+admin.add_view(ModelView(ArchaeologicalSite, db.session, name='Sites'))
+admin.add_view(ModelView(Researcher, db.session, endpoint='researchers'))
+admin.add_view(ModelView(Grave, db.session))
+admin.add_view(ModelView(Comment, db.session))
+admin.add_view(ModelView(File, db.session, endpoint='files'))
+admin.add_view(ModelView(FederalDistrict, db.session, name='Districts'))
 admin.add_view(ModelView(Region, db.session))
 admin.add_view(ModelView(Epoch, db.session))
+admin.add_view(ModelView(Preservation, db.session))
+admin.add_view(ModelView(Sex, db.session))
