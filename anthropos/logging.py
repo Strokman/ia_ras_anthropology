1d29b4773e6f9208ffcbb142c8e6a84be288abfe
Anton Strokov <anton.strokoff@gmail.com>
2023-08-12 17:09:04 +0300

    logging disabled

--- anthropos/logging.py
+++ anthropos/logging.py
@@ -1,27 +1,27 @@
-import logging
-from logging.config import dictConfig
+# import logging
+# from logging.config import dictConfig
 
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(created)f] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }, "file": {
-                "class": "logging.handlers.TimedRotatingFileHandler",
-                "filename": "flask.log",
-                "when": "D",
-                "interval": 10,
-                "backupCount": 5,
-                "formatter": "default",
-            }, },
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi', 'file']
-    }
-})
+# dictConfig({
+#     'version': 1,
+#     'formatters': {'default': {
+#         'format': '[%(created)f] %(levelname)s in %(module)s: %(message)s',
+#     }},
+#     'handlers': {'wsgi': {
+#         'class': 'logging.StreamHandler',
+#         'stream': 'ext://flask.logging.wsgi_errors_stream',
+#         'formatter': 'default'
+#     }, "file": {
+#                 "class": "logging.handlers.TimedRotatingFileHandler",
+#                 "filename": "flask.log",
+#                 "when": "D",
+#                 "interval": 10,
+#                 "backupCount": 5,
+#                 "formatter": "default",
+#             }, },
+#     'root': {
+#         'level': 'INFO',
+#         'handlers': ['wsgi', 'file']
+#     }
+# })
 
-root = logging.getLogger("root")
+# root = logging.getLogger("root")
