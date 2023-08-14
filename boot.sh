763301a81b6a9f5749ca79c201d22ae1309922e9
Anton Strokov <anton.strokoff@gmail.com>
2023-08-14 11:52:28 +0300

    21

--- boot.sh
+++ boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source venv/bin/activate
-flask db migrate
+mkdir anthropos/static/files
 flask db upgrade
 venv/bin/python create_tables.py
 exec gunicorn -b :5100 --access-logfile - --error-logfile - base_habilis:app
