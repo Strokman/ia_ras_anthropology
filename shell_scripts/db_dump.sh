4ad5bf73d13984759b2e0869bf1b0eb69732ec5b
Anton Strokov <anton.strokoff@gmail.com>
2023-08-31 09:14:56 +0300

    shell scripts fix

--- shell_scripts/db_dump.sh
+++ shell_scripts/db_dump.sh
@@ -2,10 +2,7 @@
 export PGPASSWORD="test"
 source /Users/antonstrokov/VSCode/ia_ras_anthropology/.env
 
-filename="$(date +'%d-%m-%Y-%H-%M').dump"
+filename="$(date +'%Y-%m-%d-%H-%M').dump"
 
-# mkdir ~/pg_dumps
-
+#on the prod - give full path mandatory
 pg_dump -h $POSTGRES_HOST -p $POSTGRES_PORT -Fc -U $POSTGRES_USER $POSTGRES_DB > ~/pg_dumps/$filename
-
-echo "Finished"
