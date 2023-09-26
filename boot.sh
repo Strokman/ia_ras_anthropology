e09c143956628307328f1f41e219a4a9b7da1158
Anton Strokov <anton.strokoff@gmail.com>
2023-09-26 23:02:03 +0300

    boot fix

--- boot.sh
+++ boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source venv/bin/activate
-export FLASK_ENV=dev
+export FLASK_ENV=prod
 flask db upgrade
 flask exec create-tables
 exec gunicorn -b :5000 --access-logfile - --error-logfile - base_habilis:app
