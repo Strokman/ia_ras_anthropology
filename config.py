d0e0ed9cfaab6a1cfcfc10ec3588e8f33aee3cbb
Anton Strokov <anton.strokoff@gmail.com>
2023-09-26 23:11:54 +0300

    22

--- config.py
+++ config.py
@@ -4,12 +4,8 @@ from datetime import timedelta
 
 
 basedir = path.abspath(path.dirname(__file__))
-if environ.get('FLASK_ENV') == 'dev':
-    load_dotenv(path.join(basedir, '.env.dev'))
-elif environ.get('FLASK_ENV') == 'prod':
-    load_dotenv(path.join(basedir, '.env.prod'))
-else:
-    pass
+load_dotenv(path.join(basedir, '.env'))
+
 
 
 class Config:
