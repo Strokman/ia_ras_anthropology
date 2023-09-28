3a08df565288c72438f90d148a1f7bb5124b9c19
Anton Strokov <anton.strokoff@gmail.com>
2023-09-28 09:50:21 +0300

    shell scripts fixes

--- shell_scripts/db_dump.sh
+++ shell_scripts/db_dump.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ENV_FILE="~/ia_ras_anthropology/.env"
+ENV_FILE="$(HOME)/ia_ras_anthropology/.env"
 
 source $ENV_FILE
 
 FILENAME="$(date +'%Y-%m-%d-%H-%M').dump"
-DIR="~/basehabilis_dumps"
+DIR="$(HOME)/basehabilis_dumps"
 CONTAINER="19f05141d779"
 
 sudo docker exec $CONTAINER pg_dump -Fc -U $POSTGRES_USER $POSTGRES_DB > $DIR/$FILENAME
