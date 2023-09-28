36f315c9ba5a00233cd0f3f1971554cc8dcd5dd0
Anton Strokov <anton.strokoff@gmail.com>
2023-09-28 09:52:07 +0300

    sd

--- shell_scripts/db_dump.sh
+++ shell_scripts/db_dump.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo &HOME
-ENV_FILE="$(HOME)/ia_ras_anthropology/.env"
+ENV_FILE="${HOME}/ia_ras_anthropology/.env"
 
 source $ENV_FILE
 
 FILENAME="$(date +'%Y-%m-%d-%H-%M').dump"
-DIR="$(HOME)/basehabilis_dumps"
+DIR="${HOME}/basehabilis_dumps"
 CONTAINER="19f05141d779"
 
 sudo docker exec $CONTAINER pg_dump -Fc -U $POSTGRES_USER $POSTGRES_DB > $DIR/$FILENAME
