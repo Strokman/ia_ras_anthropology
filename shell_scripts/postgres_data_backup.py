76fba3777ff29c4dd84e14f55eb52e3f9c09a657
Anton Strokov <anton.strokoff@gmail.com>
2023-09-29 14:19:01 +0300

    pg data backup

---
+++ shell_scripts/postgres_data_backup.py
@@ -0,0 +1,9 @@
+import os
+from zipfile import ZipFile
+from datetime import datetime
+path = '/home/basehabilis/ia_ras_anthropology/'
+os.chdir(path)
+filename = datetime.now().strftime('%Y-%m-%d-%H-%M') + '-pg-data' + '.zip'
+
+with ZipFile(filename, 'w') as archive:
+    archive.write('postgres-data')
