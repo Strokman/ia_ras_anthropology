5c8cb3b8ff76ae11a6e8a4941099ce367472b466
Anton Strokov <anton.strokoff@gmail.com>
2023-09-29 15:06:17 +0300

    ee

--- shell_scripts/postgres_data_backup.py
+++ shell_scripts/postgres_data_backup.py
@@ -6,6 +6,6 @@ os.chdir(path)
 filename = datetime.now().strftime('%Y-%m-%d-%H-%M') + '-pg-data' + '.zip'
 with ZipFile(f'/home/basehabilis/{filename}', 'w', ZIP_DEFLATED) as archive:
     for dirname, subdirs, files in os.walk(path):
-        archive.write(dirname, arcname='')
+        archive.write(dirname, arcname=dirname)
         for filename in files:
-            archive.write(os.path.join(dirname, filename), arcname='')
+            archive.write(name := os.path.join(dirname, filename), arcname=name)
