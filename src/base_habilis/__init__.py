c0ab301265ff2a6ca47a28437a41f8d9f145145b
Anton Strokov <anton.strokoff@gmail.com>
2023-10-06 18:51:49 +0300

    sdaasd

--- src/base_habilis/__init__.py
+++ src/base_habilis/__init__.py
@@ -22,7 +22,7 @@ from src.base_habilis.map import bp as map_bp
 from src.base_habilis.researcher import bp as researcher_bp
 from src.base_habilis.site import bp as site_bp
 from src.base_habilis.user import bp as user_bp
-from src.base_habilis.logging import dictConfig
+# from src.base_habilis.logging import dictConfig
 # import logging
 # import sys
 
