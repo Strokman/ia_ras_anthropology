bb5e6ee9ef89073dd45cd140a7d0ce71c2397eea
Anton Strokov <anton.strokoff@gmail.com>
2023-10-06 20:27:18 +0300

    c

--- src/base_habilis/logging.py
+++ src/base_habilis/logging.py
@@ -2,7 +2,7 @@ from logging.config import dictConfig
 import logging
 
 logging.StreamHandler
-log_format = '%(asctime)s - %(levelname)s in %(module)s: %(message)s'
+log_format = '%(asctime)s - %(name)s - %(levelname)s - %(module)s - %(message)s'
 
 dictConfig({
     'version': 1,
@@ -10,12 +10,11 @@ dictConfig({
         'format': log_format, 'datefmt': '%Y-%m-%d %H:%M:%S',
     }},
     'handlers': {'wsgi': {
-        'level': 'INFO',
         'class': 'logging.StreamHandler',
         'formatter': 'default'
     }, },
     'root': {
-        'level': 'INFO',
+        'level': 'DEBUG',
         'handlers': ['wsgi']
     }
 })
