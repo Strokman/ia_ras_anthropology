322f07b499d807ed8ccd40c999cdedebc1eba911
Anton Strokov <anton.strokoff@gmail.com>
2023-10-09 11:47:24 +0300

    test

--- src/base_habilis/__init__.py
+++ src/base_habilis/__init__.py
@@ -23,11 +23,29 @@ from src.base_habilis.researcher import bp as researcher_bp
 from src.base_habilis.site import bp as site_bp
 from src.base_habilis.user import bp as user_bp
 
-from logging.config import dictConfig
+# from logging.config import dictConfig
 
 # from src.base_habilis.logging import log_conf
 
 # dictConfig(log_conf)
+
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 
 def create_app(config_class: Config = Config) -> Flask:
