20b20064288dfec5ac9d4574d69ef23257162385
Anton Strokov <anton.strokoff@gmail.com>
2023-10-09 14:07:27 +0300

    try

--- src/base_habilis/__init__.py
+++ src/base_habilis/__init__.py
@@ -29,6 +29,25 @@ from src.base_habilis.user import bp as user_bp
 
 # dictConfig(log_conf)
 
+# from logging.config import dictConfig
+
+# dictConfig({
+#     'version': 1,
+#     'formatters': {'default': {
+#         'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+#     }},
+#     'handlers': {'wsgi': {
+#         'class': 'logging.StreamHandler',
+#         'stream': 'ext://flask.logging.wsgi_errors_stream',
+#         'formatter': 'default'
+#     }},
+#     'root': {
+#         'level': 'DEBUG',
+#         'handlers': ['wsgi']
+#     }
+# })
+
+
 def create_app(config_class: Config = Config) -> Flask:
 
     app = Flask(__name__)
