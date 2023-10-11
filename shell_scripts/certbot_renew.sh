e95fc562c4475f0c45f1c0632818674cb3b6c78c
Anton Strokov <anton.strokoff@gmail.com>
2023-10-11 12:10:09 +0300

    fixes

--- shell_scripts/certbot_renew.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="${HOME}/ia_ras_anthropology"
-
-cd $DIR
-
-sudo docker compose run --rm certbot renew --cert-name base-habilis.ru
--- shell_scripts/db_dump.sh
+++ shell_scripts/db_dump.sh
@@ -6,6 +6,6 @@ source $ENV_FILE
 
 FILENAME="$(date +'%Y-%m-%d-%H-%M').dump"
 DIR="${HOME}/basehabilis_dumps"
-CONTAINER="19f05141d779"
+CONTAINER="ia_ras_anthropology-db-1"
 
 sudo docker exec $CONTAINER pg_dump -Fc -U $POSTGRES_USER $POSTGRES_DB > $DIR/$FILENAME
