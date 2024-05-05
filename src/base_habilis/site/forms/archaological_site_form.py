597745a3e2478a14d9a23c78b0e58b7e03fedd67
Anton Strokov <anton.strokoff@gmail.com>
2024-05-05 17:48:50 +0200

    fix

--- src/base_habilis/site/forms/archaological_site_form.py
+++ src/base_habilis/site/forms/archaological_site_form.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed, FileField
 from wtforms import StringField, SubmitField, DecimalField, SelectField
 from wtforms.validators import NumberRange, Length, Optional
-from ...lib.validators import CleanString, SelectFieldValidator, DataRequiredImproved
+from ...lib.validators import CleanString, DataRequiredImproved
 from wtforms_sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
 from src.repository.models import Epoch, Researcher
 
@@ -24,5 +24,5 @@ class ArchaeologicalSiteForm(FlaskForm):
     long = DecimalField(places=6, label='Долгота', validators=[NumberRange(min=-180, max=180, message='Градусы должны быть в пределах -180 - 180'), DataRequiredImproved()])
     epoch = QuerySelectMultipleField('Эпохи', validators=[Optional()])
     researcher = QuerySelectField('Исследователи', allow_blank=True, blank_text='Выберите исследователя', validators=[DataRequiredImproved()]) # if possibility of multiple selection will be added - just convert to QuerySelectMultipleField and remove blank option
-    file = FileField(label='Файл с доп. информацией: pdf, xls, doc', validators=[Optional(), FileAllowed(['xls', 'xlsx', 'doc', 'pdf'], 'Формат файла не поддерживается')])
+    file = FileField(label='Файл с доп. информацией: pdf, xls, doc', validators=[Optional(), FileAllowed(['xls', 'xlsx', 'doc', 'pdf', 'docx'], 'Формат файла не поддерживается')])
     submit = SubmitField('Добавить памятник')
